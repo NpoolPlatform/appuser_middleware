8fd784193841c154f2f2a3eae260cbc3f4a38f7b
Zhao KK <kikakkz@hotmail.com>
2022-07-29 11:06:06 +0000

    Add gateway for version UT

--- cmd/appuser-middleware/run.go
+++ cmd/appuser-middleware/run.go
@@ -6,11 +6,13 @@ import (
 	"github.com/NpoolPlatform/appuser-manager/pkg/db"
 
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 
 	apimgrcli "github.com/NpoolPlatform/api-manager/pkg/client"
 
 	cli "github.com/urfave/cli/v2"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
@@ -23,11 +25,12 @@ var runCmd = &cli.Command{
 			return err
 		}
 
-		if err := grpc2.RunGRPC(rpcRegister); err != nil {
-			return err
-		}
-
-		return nil
+		go func() {
+			if err := grpc2.RunGRPC(rpcRegister); err != nil {
+				logger.Sugar().Errorf("fail to run grpc server: %v", err)
+			}
+		}()
+		return grpc2.RunGRPCGateWay(rpcGatewayRegister)
 	},
 }
 
@@ -36,5 +39,15 @@ func rpcRegister(server grpc.ServiceRegistrar) error {
 
 	apimgrcli.RegisterGRPC(server)
 
+	return nil
+}
+
+func rpcGatewayRegister(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
+	err := api.RegisterGateway(mux, endpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	apimgrcli.Register(mux)
 	return nil
 }
