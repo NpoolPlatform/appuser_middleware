8c0e1cee0dff90b86a968c4a90a30f68623072ea
Zhao KK <kikakkz@hotmail.com>
2022-07-29 05:32:27 +0000

    Add create and query

---
+++ api/app/create.go
@@ -0,0 +1,11 @@
+package app
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/app"
+)
+
+func (s *Server) CreateApp(ctx context.Context, in *npool.CreateAppRequest) (*npool.CreateAppResponse, error) {
+	return nil, nil
+}
