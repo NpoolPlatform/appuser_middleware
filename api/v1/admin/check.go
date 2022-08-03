447b9cb5deb372e71297e1ba95d465b1975317a6
fxnt <1330672764@qq.com>
2022-08-03 10:53:45 +0800

    update validate

--- api/v1/admin/check.go
+++ api/v1/admin/check.go
@@ -2,12 +2,15 @@ package admin
 
 import (
 	"context"
+	"github.com/google/uuid"
 
-	appusergrpc "github.com/NpoolPlatform/appuser-manager/pkg/client/appuser"
+	approlegrpc "github.com/NpoolPlatform/appuser-manager/pkg/client/approle"
+	approleusergrpc "github.com/NpoolPlatform/appuser-manager/pkg/client/approleuser"
 	constant "github.com/NpoolPlatform/appuser-middleware/pkg/const"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	"github.com/NpoolPlatform/message/npool"
-	appuserpb "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/appuser"
+	approlepb "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/approle"
+	approleuserpb "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/approleuser"
 	"github.com/NpoolPlatform/message/npool/appuser/mw/v1/admin"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,14 +21,28 @@ func validate(ctx context.Context, info *admin.CreateGenesisUserRequest) error {
 		return status.Error(codes.PermissionDenied, "invalid app id for genesis user")
 	}
 
-	exist, err := appusergrpc.ExistAppUserConds(ctx, &appuserpb.Conds{
+	resp, err := approlegrpc.GetAppRoleOnly(ctx, &approlepb.Conds{
 		AppID: &npool.StringVal{
 			Op:    cruder.EQ,
-			Value: info.GetAppID(),
+			Value: uuid.UUID{}.String(),
 		},
-		EmailAddress: &npool.StringVal{
+		Role: &npool.StringVal{
 			Op:    cruder.EQ,
-			Value: info.GetEmailAddress(),
+			Value: constant.GenesisRole,
+		},
+	})
+	if err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	exist, err := approleusergrpc.ExistAppRoleUserConds(ctx, &approleuserpb.Conds{
+		AppID: &npool.StringVal{
+			Op:    cruder.EQ,
+			Value: uuid.UUID{}.String(),
+		},
+		RoleID: &npool.StringVal{
+			Op:    cruder.EQ,
+			Value: resp.ID,
 		},
 	})
 	if err != nil {
