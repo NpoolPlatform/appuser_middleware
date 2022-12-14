2a6fefd0672f6d839ffeec6deedf03053da8bdf0
Zhao KK <kikakkz@hotmail.com>
2022-12-14 03:17:39 +0000

    Avoid duplicate subscriber

--- api/v1/subscriber/create.go
+++ api/v1/subscriber/create.go
@@ -1,12 +1,16 @@
-//nolint:dupl
 package subscriber
 
 import (
 	"context"
 
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	commonpb "github.com/NpoolPlatform/message/npool"
+
 	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/subscriber"
 
 	mgrapi "github.com/NpoolPlatform/appuser-manager/api/v2/subscriber"
+	mgrcli "github.com/NpoolPlatform/appuser-manager/pkg/client/subscriber"
+	mgrpb "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/subscriber"
 
 	subscriber1 "github.com/NpoolPlatform/appuser-middleware/pkg/subscriber"
 
@@ -19,12 +23,35 @@ func (s *Server) CreateSubscriber(ctx context.Context, in *npool.CreateSubscribe
 		return &npool.CreateSubscriberResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	info, err := subscriber1.CreateSubscriber(ctx, in.GetInfo())
+	info, err := mgrcli.GetSubscriberOnly(ctx, &mgrpb.Conds{
+		AppID: &commonpb.StringVal{
+			Op:    cruder.EQ,
+			Value: in.GetInfo().GetAppID(),
+		},
+		EmailAddress: &commonpb.StringVal{
+			Op:    cruder.EQ,
+			Value: in.GetInfo().GetEmailAddress(),
+		},
+	})
+	if err != nil {
+		return &npool.CreateSubscriberResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	if info != nil {
+		info1, err := subscriber1.GetSubscriber(ctx, info.ID)
+		if err != nil {
+			return &npool.CreateSubscriberResponse{}, status.Error(codes.Internal, err.Error())
+		}
+		return &npool.CreateSubscriberResponse{
+			Info: info1,
+		}, nil
+	}
+
+	info1, err := subscriber1.CreateSubscriber(ctx, in.GetInfo())
 	if err != nil {
 		return &npool.CreateSubscriberResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
 	return &npool.CreateSubscriberResponse{
-		Info: info,
+		Info: info1,
 	}, nil
 }
