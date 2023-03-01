29f2a9e4bed713c0c957887b5663e744c6f6c758
Zhao KK <kikakkz@hotmail.com>
2023-03-01 08:20:18 +0000

    Support query action credits

--- pkg/user/query.go
+++ pkg/user/query.go
@@ -269,7 +269,7 @@ func expand(ctx context.Context, userIDs []string, users []*user.User) ([]*user.
 	}
 
 	for _, user := range users {
-		if credits, err := decimal.NewFromString(user.ActionCredits); err != nil {
+		if credits, err := decimal.NewFromString(user.ActionCredits); err == nil {
 			user.ActionCredits = credits.String()
 		} else {
 			user.ActionCredits = decimal.NewFromInt(0).String()
