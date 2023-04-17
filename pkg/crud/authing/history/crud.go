9be217b90eacb674776cc1f4256a97724dce0fc3
Zhao KK <kikakkz@hotmail.com>
2023-04-17 15:35:12 +0000

    Pass crud lint

--- pkg/crud/authing/history/crud.go
+++ pkg/crud/authing/history/crud.go
@@ -1,118 +1,135 @@
 package history
 
 import (
-	"context"
 	"fmt"
-	"time"
 
-	"github.com/NpoolPlatform/appuser-middleware/pkg/db"
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db/ent"
-
-	"github.com/NpoolPlatform/appuser-middleware/pkg/db/ent/authhistory"
+	entauthhistory "github.com/NpoolPlatform/appuser-middleware/pkg/db/ent/authhistory"
 
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	"github.com/google/uuid"
 )
 
-func CreateSet(c *ent.AuthHistoryCreate, info *npool.HistoryReq) *ent.AuthHistoryCreate {
-	if info.ID != nil {
-		c.SetID(uuid.MustParse(info.GetID()))
+type Req struct {
+	ID       *uuid.UUID
+	AppID    *uuid.UUID
+	UserID   *uuid.UUID
+	Resource *string
+	Method   *string
+	Allowed  *bool
+}
+
+func CreateSet(c *ent.AuthHistoryCreate, req *Req) *ent.AuthHistoryCreate {
+	if req.ID != nil {
+		c.SetID(*req.ID)
 	}
-	if info.AppID != nil {
-		c.SetAppID(uuid.MustParse(info.GetAppID()))
+	if req.AppID != nil {
+		c.SetAppID(*req.AppID)
 	}
-	if info.UserID != nil {
-		c.SetUserID(uuid.MustParse(info.GetUserID()))
+	if req.UserID != nil {
+		c.SetUserID(*req.UserID)
 	}
-	if info.Resource != nil {
-		c.SetResource(info.GetResource())
+	if req.Resource != nil {
+		c.SetResource(*req.Resource)
 	}
-	if info.Method != nil {
-		c.SetMethod(info.GetMethod())
+	if req.Method != nil {
+		c.SetMethod(*req.Method)
 	}
-	if info.Allowed != nil {
-		c.SetAllowed(info.GetAllowed())
+	if req.Allowed != nil {
+		c.SetAllowed(*req.Allowed)
 	}
 	return c
 }
 
-func UpdateSet(u *ent.AuthHistoryUpdateOne, in *npool.HistoryReq) *ent.AuthHistoryUpdateOne {
+func UpdateSet(u *ent.AuthHistoryUpdateOne, in *Req) *ent.AuthHistoryUpdateOne {
 	return u
 }
 
-//nolint:nolintlint,gocyclo
-func SetQueryConds(conds *npool.Conds, cli *ent.Client) (*ent.AuthHistoryQuery, error) {
-	stm := cli.AuthHistory.Query()
+type Conds struct {
+	ID       *cruder.Cond
+	AppID    *cruder.Cond
+	UserID   *cruder.Cond
+	Resource *cruder.Cond
+	Method   *cruder.Cond
+	Allowed  *cruder.Cond
+}
 
+//nolint
+func SetQueryConds(q *ent.AuthHistoryQuery, conds *Conds) (*ent.AuthHistoryQuery, error) {
 	if conds == nil {
 		return stm, nil
 	}
-
 	if conds.ID != nil {
-		id, err := uuid.Parse(conds.GetID().GetValue())
-		if err != nil {
-			return nil, err
+		id, ok := conds.ID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid id")
 		}
-		switch conds.GetID().GetOp() {
+		switch conds.ID.Op {
 		case cruder.EQ:
-			stm.Where(authhistory.ID(id))
+			stm.Where(entauthhistory.ID(id))
 		default:
 			return nil, fmt.Errorf("invalid app field")
 		}
 	}
-
 	if conds.AppID != nil {
-		appID, err := uuid.Parse(conds.GetAppID().GetValue())
-		if err != nil {
-			return nil, err
+		id, ok := conds.AppID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid appid")
 		}
-		switch conds.GetAppID().GetOp() {
+		switch conds.AppID.Op {
 		case cruder.EQ:
-			stm.Where(authhistory.AppID(appID))
+			stm.Where(entauthhistory.AppID(id))
 		default:
 			return nil, fmt.Errorf("invalid app field")
 		}
 	}
-
 	if conds.UserID != nil {
-		userID, err := uuid.Parse(conds.GetUserID().GetValue())
-		if err != nil {
-			return nil, err
+		id, ok := conds.UserID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid userid")
 		}
-		switch conds.GetUserID().GetOp() {
+		switch conds.UserID.Op {
 		case cruder.EQ:
-			stm.Where(authhistory.UserID(userID))
+			stm.Where(entauthhistory.UserID(id))
 		default:
 			return nil, fmt.Errorf("invalid app field")
 		}
 	}
-
 	if conds.Resource != nil {
-		switch conds.GetResource().GetOp() {
+		res, ok := conds.Resource.Val.(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid resource")
+		}
+		switch conds.Resource.Op {
 		case cruder.EQ:
-			stm.Where(authhistory.Resource(conds.GetResource().GetValue()))
+			stm.Where(entauthhistory.Resource(res))
 		default:
 			return nil, fmt.Errorf("invalid app field")
 		}
 	}
-
 	if conds.Method != nil {
-		switch conds.GetMethod().GetOp() {
+		method, ok := conds.Method.Val.(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid method")
+		}
+		switch conds.Method.Op {
 		case cruder.EQ:
-			stm.Where(authhistory.Method(conds.GetMethod().GetValue()))
+			stm.Where(entauthhistory.Method(method))
 		default:
 			return nil, fmt.Errorf("invalid app field")
 		}
 	}
-
 	if conds.Allowed != nil {
-		switch conds.GetAllowed().GetOp() {
+		allowed, ok := conds.Method.Val.(bool)
+		if !ok {
+			return nil, fmt.Errorf("invalid allowed")
+		}
+		switch conds.Allowed.Op {
 		case cruder.EQ:
-			stm.Where(authhistory.Allowed(conds.GetAllowed().GetValue()))
+			stm.Where(entauthhistory.Allowed(allowed))
 		default:
 			return nil, fmt.Errorf("invalid app field")
 		}
 	}
-
 	return stm, nil
 }
