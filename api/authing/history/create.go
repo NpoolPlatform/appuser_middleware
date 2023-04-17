8d67bbf0ca2df7d86c1f132e91fbe74ab01c42f3
Zhao KK <kikakkz@hotmail.com>
2023-04-17 14:49:19 +0000

    Better handler definition

--- api/authing/history/create.go
+++ api/authing/history/create.go
@@ -1,1 +1,43 @@
 package history
+
+/*
+import (
+	"context"
+
+	history1 "github.com/NpoolPlatform/appuser-middleware/pkg/mw/authing/history"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/authing/history"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) CreateHistory(ctx context.Context, in *npool.CreateHistoryRequest) (*npool.CreateHistoryResponse, error) {
+	handler, err := history1.NewHandler(
+		ctx,
+		history1.WithConds(in.GetConds()),
+		history1.WithOffset(in.GetOffset()),
+		history1.WithLimit(in.GetLimit()),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetAuths",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.CreateHistoryResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+	info, err := handler.CreateHistory(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetAuths",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.CreateHistoryResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+	return &npool.CreateHistoryResponse{
+		Info: info,
+	}, nil
+}
+*/
