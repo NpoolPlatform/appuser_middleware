5164d5d93685280e6003fac9330355f560010081
Zhao KK <kikakkz@hotmail.com>
2023-04-18 10:17:26 +0000

    Add crud for role and roleuser

--- api/role/user/create.go
+++ api/role/user/create.go
@@ -1,1 +1,43 @@
 package user
+
+import (
+	"context"
+
+	user1 "github.com/NpoolPlatform/appuser-middleware/pkg/mw/role/user"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/role/user"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) CreateUser(ctx context.Context, in *npool.CreateUserRequest) (*npool.CreateUserResponse, error) {
+	req := in.GetInfo()
+	handler, err := user1.NewHandler(
+		ctx,
+		user1.WithID(req.ID),
+		user1.WithAppID(req.GetAppID()),
+		user1.WithRoleID(req.RoleID),
+		user1.WithUserID(req.UserID),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateUser",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.CreateUserResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+	info, err := handler.CreateUser(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateUser",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.CreateUserResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+	return &npool.CreateUserResponse{
+		Info: info,
+	}, nil
+}
---
+++ pkg/mw/role/user/delete.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/NpoolPlatform/appuser-middleware/pkg/db"
+	"github.com/NpoolPlatform/appuser-middleware/pkg/db/ent"
+
+	usercrud "github.com/NpoolPlatform/appuser-middleware/pkg/crud/role/user"
+	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/role/user"
+)
+
+func (h *Handler) DeleteUser(ctx context.Context) (*npool.User, error) {
+	if h.ID == nil {
+		return nil, fmt.Errorf("invalid id")
+	}
+
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		now := uint32(time.Now().Unix())
+		if _, err := usercrud.UpdateSet(
+			cli.AppRoleUser.UpdateOneID(*h.ID),
+			&usercrud.Req{
+				ID:        h.ID,
+				DeletedAt: &now,
+			},
+		).Save(ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return h.GetUser(ctx)
+}
