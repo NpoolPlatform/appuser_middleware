2e62b991d038ee69e41fa1cc415eba916ba38953
Zhao KK <kikakkz@hotmail.com>
2023-04-18 01:52:57 +0000

    Add basic UT test file

---
+++ pkg/mw/authing/auth/auth_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/NpoolPlatform/appuser-middleware/pkg/testinit"
+)
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
+
+func createAuth(t *testing.T) {
+
+}
+
+func TestMainOrder(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	t.Run("createAuth", createAuth)
+}
