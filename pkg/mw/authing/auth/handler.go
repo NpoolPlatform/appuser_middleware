ab4be10d65067c422461848fcb7e14fc3ca91186
Zhao KK <kikakkz@hotmail.com>
2023-04-18 01:29:31 +0000

    Add inheritable handler

--- pkg/mw/authing/auth/handler.go
+++ pkg/mw/authing/auth/handler.go
@@ -1,9 +1,15 @@
 package auth
 
 import (
+	"context"
+
 	handler "github.com/NpoolPlatform/appuser-middleware/pkg/mw/authing/handler"
 )
 
 type Handler struct {
 	*handler.Handler
+}
+
+func (h *Handler) VerifyConds(ctx context.Context, conds interface{}) error {
+	return nil
 }
