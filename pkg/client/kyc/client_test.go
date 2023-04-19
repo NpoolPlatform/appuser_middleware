f2df39d223e7fac6e82b821420fd5a306f7acf1f
Zhao KK <kikakkz@hotmail.com>
2023-04-19 04:16:51 +0000

    Add kyc unit test

--- pkg/client/kyc/client_test.go
+++ pkg/client/kyc/client_test.go
@@ -1,30 +1,28 @@
 package kyc
 
-/*
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"testing"
 
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	"github.com/NpoolPlatform/message/npool"
-
-	mgr "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/kyc"
-
 	"bou.ke/monkey"
-	kyccli "github.com/NpoolPlatform/appuser-manager/pkg/crud/kyc"
 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-	"github.com/NpoolPlatform/message/npool/appuser/mw/v1/kyc"
+	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/kyc"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 
+	app "github.com/NpoolPlatform/appuser-middleware/pkg/mw/app"
+	user "github.com/NpoolPlatform/appuser-middleware/pkg/mw/user"
 	"github.com/NpoolPlatform/appuser-middleware/pkg/testinit"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 )
 
 func init() {
@@ -37,65 +35,134 @@ func init() {
 }
 
 var (
-	state   = mgr.KycState_Reviewing
-	kycInfo = kyc.Kyc{
+	ret = npool.Kyc{
 		ID:              uuid.NewString(),
 		AppID:           uuid.NewString(),
 		UserID:          uuid.NewString(),
-		DocumentType:    mgr.KycDocumentType_IDCard,
-		DocumentTypeStr: mgr.KycDocumentType_IDCard.String(),
+		DocumentType:    basetypes.KycDocumentType_IDCard,
+		DocumentTypeStr: basetypes.KycDocumentType_IDCard.String(),
 		IDNumber:        uuid.NewString(),
 		FrontImg:        uuid.NewString(),
 		BackImg:         uuid.NewString(),
 		SelfieImg:       uuid.NewString(),
-		EntityType:      mgr.KycEntityType_Individual,
-		EntityTypeStr:   mgr.KycEntityType_Individual.String(),
+		EntityType:      basetypes.KycEntityType_Individual,
+		EntityTypeStr:   basetypes.KycEntityType_Individual.String(),
 		ReviewID:        uuid.NewString(),
-		StateStr:        mgr.KycState_Reviewing.String(),
-		State:           mgr.KycState_Reviewing,
+		State:           basetypes.KycState_Reviewing,
+		StateStr:        basetypes.KycState_Reviewing.String(),
 	}
 )
 
-func create(t *testing.T) {
-	appRoleReq := mgr.KycReq{
-		ID:           &kycInfo.ID,
-		AppID:        &kycInfo.AppID,
-		UserID:       &kycInfo.UserID,
-		DocumentType: &kycInfo.DocumentType,
-		IDNumber:     &kycInfo.IDNumber,
-		FrontImg:     &kycInfo.FrontImg,
-		BackImg:      &kycInfo.BackImg,
-		SelfieImg:    &kycInfo.SelfieImg,
-		EntityType:   &kycInfo.EntityType,
-		ReviewID:     &kycInfo.ReviewID,
-		State:        &state,
+func setupKyc(t *testing.T) func(*testing.T) {
+	ah, err := app.NewHandler(
+		context.Background(),
+		app.WithID(&ret.AppID),
+		app.WithCreatedBy(ret.UserID),
+		app.WithName(&ret.AppID),
+	)
+	assert.Nil(t, err)
+	assert.NotNil(t, ah)
+	app1, err := ah.CreateApp(context.Background())
+	assert.Nil(t, err)
+	assert.NotNil(t, app1)
+
+	ret.PhoneNO = fmt.Sprintf("+86%v", rand.Intn(100000000)+1000000)           //nolint
+	ret.EmailAddress = fmt.Sprintf("%v@hhh.ccc", rand.Intn(100000000)+1000000) //nolint
+	passwordHash := uuid.NewString()
+
+	ret.AppName = ret.AppID
+
+	uh, err := user.NewHandler(
+		context.Background(),
+		user.WithID(&ret.UserID),
+		user.WithAppID(ret.GetAppID()),
+		user.WithPhoneNO(&ret.PhoneNO),
+		user.WithEmailAddress(&ret.EmailAddress),
+		user.WithPasswordHash(&passwordHash),
+	)
+	assert.Nil(t, err)
+	assert.NotNil(t, uh)
+	user1, err := uh.CreateUser(context.Background())
+	assert.Nil(t, err)
+	assert.NotNil(t, user1)
+
+	return func(*testing.T) {
+		_, _ = ah.DeleteApp(context.Background())
+		_, _ = uh.DeleteUser(context.Background())
 	}
-	_, err := kyccli.Create(context.Background(), &appRoleReq)
-	if !assert.Nil(t, err) {
-		return
+}
+
+func createKyc(t *testing.T) {
+	req := npool.KycReq{
+		ID:           &ret.ID,
+		AppID:        &ret.AppID,
+		UserID:       &ret.UserID,
+		DocumentType: &ret.DocumentType,
+		IDNumber:     &ret.IDNumber,
+		FrontImg:     &ret.FrontImg,
+		BackImg:      &ret.BackImg,
+		SelfieImg:    &ret.SelfieImg,
+		EntityType:   &ret.EntityType,
+		ReviewID:     &ret.ReviewID,
+		State:        &ret.State,
+	}
+	info, err := CreateKyc(context.Background(), &req)
+	if assert.Nil(t, err) {
+		ret.CreatedAt = info.CreatedAt
+		ret.UpdatedAt = info.UpdatedAt
+		assert.Equal(t, info, &ret)
+	}
+}
+
+func updateKyc(t *testing.T) {
+	ret.IDNumber = uuid.NewString()
+	ret.FrontImg = uuid.NewString()
+	ret.State = basetypes.KycState_Rejected
+	ret.StateStr = basetypes.KycState_Rejected.String()
+
+	req := npool.KycReq{
+		ID:           &ret.ID,
+		DocumentType: &ret.DocumentType,
+		IDNumber:     &ret.IDNumber,
+		FrontImg:     &ret.FrontImg,
+		BackImg:      &ret.BackImg,
+		SelfieImg:    &ret.SelfieImg,
+		EntityType:   &ret.EntityType,
+		ReviewID:     &ret.ReviewID,
+		State:        &ret.State,
+	}
+	info, err := UpdateKyc(context.Background(), &req)
+	if assert.Nil(t, err) {
+		ret.UpdatedAt = info.UpdatedAt
+		assert.Equal(t, info, &ret)
 	}
 }
 
 func getKyc(t *testing.T) {
-	info, err := GetKyc(context.Background(), kycInfo.GetID())
+	info, err := GetKyc(context.Background(), ret.ID)
 	if assert.Nil(t, err) {
-		kycInfo.CreatedAt = info.CreatedAt
-		kycInfo.UpdatedAt = info.UpdatedAt
-		assert.Equal(t, info, &kycInfo)
+		assert.Equal(t, info, &ret)
 	}
 }
 
 func getKycs(t *testing.T) {
-	infos, _, err := GetKycs(context.Background(), &kyc.Conds{
-		Conds: &mgr.Conds{
-			ID: &npool.StringVal{
-				Op:    cruder.EQ,
-				Value: kycInfo.ID,
-			},
-		}}, 0, 1)
+	_, total, err := GetKycs(context.Background(), &npool.Conds{
+		AppID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
+	}, 0, 1)
 	if assert.Nil(t, err) {
-		assert.NotEqual(t, len(infos), 0)
+		assert.NotEqual(t, total, 0)
 	}
+}
+
+func deleteKyc(t *testing.T) {
+	info, err := DeleteKyc(context.Background(), ret.ID)
+	if assert.Nil(t, err) {
+		assert.Equal(t, info, &ret)
+	}
+
+	info, err = GetKyc(context.Background(), ret.ID)
+	assert.Nil(t, err)
+	assert.Nil(t, info)
 }
 
 func TestMainOrder(t *testing.T) {
@@ -109,8 +176,12 @@ func TestMainOrder(t *testing.T) {
 		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	})
 
-	t.Run("create", create)
+	teardown := setupKyc(t)
+	defer teardown(t)
+
+	t.Run("createKyc", createKyc)
+	t.Run("updateKyc", updateKyc)
 	t.Run("getKyc", getKyc)
 	t.Run("getKycs", getKycs)
+	t.Run("deleteKyc", deleteKyc)
 }
-*/
