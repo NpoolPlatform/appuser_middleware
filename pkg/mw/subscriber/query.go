4d396e8c4f8cd0cf9a8153cdd425f9f09f1a2304
Zhao KK <kikakkz@hotmail.com>
2023-04-19 02:29:34 +0000

    Add subscriber middleware

--- pkg/mw/subscriber/query.go
+++ pkg/mw/subscriber/query.go
@@ -1,123 +1,134 @@
 package subscriber
 
-/*
 import (
 	"context"
 	"fmt"
-
-	mgrpb "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/subscriber"
-	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/subscriber"
 
 	"entgo.io/ent/dialect/sql"
 
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db"
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db/ent"
 
-	crud "github.com/NpoolPlatform/appuser-middleware/pkg/crud/subscriber"
-
+	subscribercrud "github.com/NpoolPlatform/appuser-middleware/pkg/crud/subscriber"
 	entapp "github.com/NpoolPlatform/appuser-middleware/pkg/db/ent/app"
 	entsubscriber "github.com/NpoolPlatform/appuser-middleware/pkg/db/ent/subscriber"
-
-	"github.com/google/uuid"
+	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/subscriber"
 )
 
-func GetSubscriber(ctx context.Context, id string) (*npool.Subscriber, error) {
-	var infos []*npool.Subscriber
+type queryHandler struct {
+	*Handler
+	stm   *ent.SubscriberSelect
+	infos []*npool.Subscriber
+	total uint32
+}
 
-	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		stm := cli.
-			Subscriber.
+func (h *queryHandler) selectSubscriber(stm *ent.SubscriberQuery) {
+	h.stm = stm.Select(
+		entsubscriber.FieldID,
+		entsubscriber.FieldAppID,
+		entsubscriber.FieldEmailAddress,
+		entsubscriber.FieldRegistered,
+		entsubscriber.FieldCreatedAt,
+		entsubscriber.FieldUpdatedAt,
+	)
+}
+
+func (h *queryHandler) querySubscriber(cli *ent.Client) error {
+	if h.ID == nil {
+		return fmt.Errorf("invalid subscriber id")
+	}
+
+	h.selectSubscriber(
+		cli.Subscriber.
 			Query().
 			Where(
-				entsubscriber.ID(uuid.MustParse(id)),
-			)
-		return join(stm).
-			Scan(_ctx, &infos)
+				entsubscriber.ID(*h.ID),
+				entsubscriber.DeletedAt(0),
+			),
+	)
+	return nil
+}
+
+func (h *queryHandler) querySubscribers(cli *ent.Client) error {
+	stm, err := subscribercrud.SetQueryConds(cli.Subscriber.Query(), h.Conds)
+	if err != nil {
+		return err
+	}
+	h.selectSubscriber(stm)
+	return nil
+}
+
+func (h *queryHandler) queryJoinApp(s *sql.Selector) {
+	t := sql.Table(entapp.Table)
+	s.LeftJoin(t).
+		On(
+			s.C(entsubscriber.FieldAppID),
+			t.C(entapp.FieldID),
+		).
+		AppendSelect(
+			sql.As(t.C(entapp.FieldName), "app_name"),
+		)
+}
+
+func (h *queryHandler) queryJoin() {
+	h.stm.Modify(func(s *sql.Selector) {
+		h.queryJoinApp(s)
+	})
+}
+
+func (h *queryHandler) scan(ctx context.Context) error {
+	return h.stm.Scan(ctx, &h.infos)
+}
+
+func (h *Handler) GetSubscriber(ctx context.Context) (*npool.Subscriber, error) {
+	handler := &queryHandler{
+		Handler: h,
+	}
+
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.querySubscriber(cli); err != nil {
+			return err
+		}
+		handler.queryJoin()
+		if err := handler.scan(ctx); err != nil {
+			return nil
+		}
+		return nil
 	})
 	if err != nil {
 		return nil, err
 	}
-	if len(infos) == 0 {
-		return nil, fmt.Errorf("no record")
+	if len(handler.infos) == 0 {
+		return nil, nil
 	}
-	if len(infos) > 1 {
+	if len(handler.infos) > 1 {
 		return nil, fmt.Errorf("too many record")
 	}
 
-	return infos[0], nil
+	return handler.infos[0], nil
 }
 
-func GetSubscriberes(ctx context.Context, conds *mgrpb.Conds, offset, limit int32) ([]*npool.Subscriber, uint32, error) {
-	var infos []*npool.Subscriber
-	var total uint32
+func (h *Handler) GetSubscribers(ctx context.Context) ([]*npool.Subscriber, uint32, error) {
+	handler := &queryHandler{
+		Handler: h,
+	}
 
 	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		stm, err := crud.SetQueryConds(conds, cli)
-		if err != nil {
+		if err := handler.querySubscribers(cli); err != nil {
 			return err
 		}
-
-		_total, err := stm.Count(ctx)
-		if err != nil {
-			return err
+		handler.queryJoin()
+		handler.stm.
+			Offset(int(h.Offset)).
+			Limit(int(h.Limit))
+		if err := handler.scan(ctx); err != nil {
+			return nil
 		}
-		total = uint32(_total)
-
-		return join(stm).
-			Scan(_ctx, &infos)
+		return nil
 	})
 	if err != nil {
 		return nil, 0, err
 	}
 
-	return infos, total, nil
+	return handler.infos, handler.total, nil
 }
-
-func GetSubscriberOnly(ctx context.Context, conds *mgrpb.Conds) (*npool.Subscriber, error) {
-	var infos []*npool.Subscriber
-
-	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		stm, err := crud.SetQueryConds(conds, cli)
-		if err != nil {
-			return err
-		}
-		return join(stm).
-			Scan(_ctx, &infos)
-	})
-	if err != nil {
-		return nil, err
-	}
-	if len(infos) == 0 {
-		return nil, nil
-	}
-	if len(infos) > 1 {
-		return nil, fmt.Errorf("too many record")
-	}
-
-	return infos[0], nil
-}
-
-func join(stm *ent.SubscriberQuery) *ent.SubscriberSelect {
-	return stm.
-		Select(
-			entsubscriber.FieldID,
-			entsubscriber.FieldAppID,
-			entsubscriber.FieldEmailAddress,
-			entsubscriber.FieldRegistered,
-			entsubscriber.FieldCreatedAt,
-			entsubscriber.FieldUpdatedAt,
-		).
-		Modify(func(s *sql.Selector) {
-			t1 := sql.Table(entapp.Table)
-			s.
-				LeftJoin(t1).
-				On(
-					s.C(entsubscriber.FieldAppID),
-					t1.C(entapp.FieldID),
-				).
-				AppendSelect(
-					sql.As(t1.C(entapp.FieldName), "app_name"),
-				)
-		})
-}
-*/
