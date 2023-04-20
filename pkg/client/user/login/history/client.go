6d225b06dddf082a3927a86fcab2b8aed00af337
Zhao KK <kikakkz@hotmail.com>
2023-04-20 02:58:52 +0000

    Add history lcient

---
+++ pkg/client/user/login/history/client.go
@@ -0,0 +1,82 @@
+//nolint:nolintlint,dupl
+package history
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/user/login/history"
+
+	servicename "github.com/NpoolPlatform/appuser-middleware/pkg/servicename"
+)
+
+func do(ctx context.Context, fn func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error)) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, 10*time.Second) //nolint
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return fn(_ctx, cli)
+}
+
+func CreateHistory(ctx context.Context, in *npool.HistoryReq) (*npool.History, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateHistory(ctx, &npool.CreateHistoryRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.History), nil
+}
+
+func GetHistory(ctx context.Context, id string) (*npool.History, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetHistory(ctx, &npool.GetHistoryRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.History), nil
+}
+
+func GetHistories(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.History, uint32, error) {
+	var total uint32
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetHistories(ctx, &npool.GetHistoriesRequest{
+			Conds:  conds,
+			Offset: offset,
+			Limit:  limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		total = resp.GetTotal()
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*npool.History), total, nil
+}
