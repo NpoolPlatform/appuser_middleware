8c0257ad17b206c2e60a5355c46e5fe64f9c55e3
Zhao KK <kikakkz@hotmail.com>
2023-04-21 03:10:44 +0000

    Add app exist api

--- pkg/mw/app/exist.go
+++ pkg/mw/app/exist.go
@@ -7,6 +7,7 @@ import (
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db"
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db/ent"
 
+	appcrud "github.com/NpoolPlatform/appuser-middleware/pkg/crud/app"
 	entapp "github.com/NpoolPlatform/appuser-middleware/pkg/db/ent/app"
 )
 
@@ -22,6 +23,24 @@ func (h *Handler) ExistApp(ctx context.Context) (exist bool, err error) {
 				entapp.ID(*h.ID),
 			).
 			Exist(_ctx)
+		return err
+	})
+	if err != nil {
+		return false, err
+	}
+	return exist, nil
+}
+
+func (h *Handler) ExistAppConds(ctx context.Context) (exist bool, err error) {
+	if h.ID == nil {
+		return false, fmt.Errorf("invalid id")
+	}
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		stm, err := appcrud.SetQueryConds(cli.App.Query(), h.Conds)
+		if err != nil {
+			return err
+		}
+		exist, err = stm.Exist(_ctx)
 		return err
 	})
 	if err != nil {
