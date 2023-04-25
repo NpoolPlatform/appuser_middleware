c952c9e5161f7892bd0f4a7ccdddce78be00a4f4
Zhao KK <kikakkz@hotmail.com>
2023-04-25 05:23:44 +0000

    Support update genesis

--- pkg/mw/role/handler.go
+++ pkg/mw/role/handler.go
@@ -126,6 +126,7 @@ func WithGenesis(genesis *bool) func(context.Context, *Handler) error {
 	}
 }
 
+//nolint:gocyclo
 func WithConds(conds *npool.Conds) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
 		h.Conds = &rolecrud.Conds{}
