d04b518e1b83199571325f569ae4dc0519ce06e4
Zhao KK <kikakkz@hotmail.com>
2023-05-13 06:18:30 +0000

    Fix: http://47.116.78.131:10083/issues/1366

--- pkg/mw/role/user/create.go
+++ pkg/mw/role/user/create.go
@@ -2,11 +2,13 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db"
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db/ent"
 
 	usercrud "github.com/NpoolPlatform/appuser-middleware/pkg/crud/role/user"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/role/user"
 
 	"github.com/google/uuid"
@@ -17,8 +19,34 @@ func (h *Handler) CreateUser(ctx context.Context) (*npool.User, error) {
 	if h.ID == nil {
 		h.ID = &id
 	}
+	if h.RoleID == nil || h.UserID == nil {
+		return nil, fmt.Errorf("invalid roleid or userid")
+	}
 
 	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		stm, err := usercrud.SetQueryConds(
+			cli.AppRoleUser.Query(),
+			&usercrud.Conds{
+				AppID:  &cruder.Cond{Op: cruder.EQ, Val: h.AppID},
+				RoleID: &cruder.Cond{Op: cruder.EQ, Val: *h.RoleID},
+				UserID: &cruder.Cond{Op: cruder.EQ, Val: *h.UserID},
+			},
+		)
+		if err != nil {
+			return err
+		}
+
+		info, err := stm.Only(_ctx)
+		if err != nil {
+			if !ent.IsNotFound(err) {
+				return err
+			}
+		}
+		if info != nil {
+			h.ID = &info.ID
+			return nil
+		}
+
 		if _, err := usercrud.CreateSet(
 			cli.AppRoleUser.Create(),
 			&usercrud.Req{
