fb373f44d241b8d21227ec0d2e3a82f592384428
Zhao KK <kikakkz@hotmail.com>
2023-06-30 09:04:12 +0000

    Add lock to create subscriber

--- pkg/mw/subscriber/create.go
+++ pkg/mw/subscriber/create.go
@@ -2,12 +2,16 @@ package subscriber
 
 import (
 	"context"
+	"fmt"
 
 	subscribercrud "github.com/NpoolPlatform/appuser-middleware/pkg/crud/subscriber"
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db"
 	"github.com/NpoolPlatform/appuser-middleware/pkg/db/ent"
 	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	npool "github.com/NpoolPlatform/message/npool/appuser/mw/v1/subscriber"
+
+	redis2 "github.com/NpoolPlatform/go-service-framework/pkg/redis"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 
 	"github.com/google/uuid"
 )
@@ -17,6 +21,14 @@ func (h *Handler) CreateSubscriber(ctx context.Context) (*npool.Subscriber, erro
 	if h.ID == nil {
 		h.ID = &id
 	}
+
+	key := fmt.Sprintf("%v:%v:%v", basetypes.Prefix_PrefixCreateSubscriber, h.AppID, *h.EmailAddress)
+	if err := redis2.TryLock(key, 0); err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = redis2.Unlock(key)
+	}()
 
 	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
 		stm, err := subscribercrud.SetQueryConds(
