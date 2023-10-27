eee7e41015e75f3b41d5dbbb2f781fc88114d816
lidaqiang <dqli161@163.com>
2023-10-27 12:11:04 +0800

    register kyc http path

--- api/kyc/api.go
+++ api/kyc/api.go
@@ -1,7 +1,10 @@
 package kyc
 
 import (
+	"context"
+
 	"github.com/NpoolPlatform/message/npool/appuser/mw/v1/kyc"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
@@ -11,4 +14,8 @@ type Server struct {
 
 func Register(server grpc.ServiceRegistrar) {
 	kyc.RegisterMiddlewareServer(server, &Server{})
+}
+
+func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
+	return kyc.RegisterMiddlewareHandlerFromEndpoint(context.Background(), mux, endpoint, opts)
 }
